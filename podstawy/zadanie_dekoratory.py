1360de5092ef5da763d1c067ab6894fc16b6594e
Konrad Gawda <k.gawda@alx.pl>
2022-04-09 10:07:04 +0200

    Zadania domowe - rozwiązania

--- podstawy/zadanie_dekoratory.py
+++ podstawy/zadanie_dekoratory.py
@@ -4,21 +4,36 @@ def main():
     #1. dekorator liczący czas
     # w środku rozwiązania znajdzie się:
 
-    time_start = time.perf_counter()
-    # wywołanie funkcji
-    time_end = time.perf_counter()
-    print("Wywołanie funkcji zajęło", time_end - time_start)
+    def czas_wywolania(f):
+        def wrapper(*args, **kwargs):
+            time_start = time.perf_counter()
+            result = f(*args, **kwargs)
+            time_end = time.perf_counter()
+            print("Wywołanie funkcji zajęło", time_end - time_start)
+            return result
+        return wrapper
+
+    @czas_wywolania
+    def powolna_funkcja(a):
+        print("Działa powolna_funkcja")
+        time.sleep(a)
+
+    powolna_funkcja(0.1)
+
 
     #2. dekorator zbierający w listę
 
     class ListaObiektow:
         def __init__(self):
             self.lista = []
+            self.slownik = {}
 
-        def dodaj_do_listy(self, x):
+        def dodaj_do_listy(self, funkcja):
             """ Dekorator, możliwy do użycia na obiekcie i funkcji.
             Nie modyfikuje obiektu/funkcji, a jedynie dodaje go do self.lista"""
-            pass # TODO
+            self.lista.append(funkcja)
+            self.slownik[funkcja.__name__] = funkcja
+            return funkcja
 
     widoki = ListaObiektow()
 
@@ -34,6 +49,7 @@ def main():
     assert greet in widoki.lista
     assert home(None) == "Witaj na stronie"
     assert greet(1) == "Witaj, twój request to 1"
+    assert widoki.slownik["home"]("cokolwiek") == "Witaj na stronie"
 
     # 3. fabryka fukncji-kluczy do sortowania
 
@@ -42,8 +58,11 @@ def main():
     l.sort(key=lambda x: x[1])  # to by posortowało po drugiej literze
 
     def fabryka_funkcji_pobierajacych_dana_litere(idx_litery):
-        pass # TODO
-        # zadanie dodatkowe: jeśli jest za mało liter to podać ostatnią
+        def pobierz_litere(s):
+            if idx_litery > len(s) - 1:
+                return s[-1]
+            return s[idx_litery]
+        return pobierz_litere
 
     l.sort(key=fabryka_funkcji_pobierajacych_dana_litere(0))  # ma sortowac po pierszej literze
     l.sort(key=fabryka_funkcji_pobierajacych_dana_litere(1))  # ma sortowac po pierszej literze
