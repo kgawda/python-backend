3a363b7050e47ade77e5e6e78d849036fc3dd2e7
Konrad Gawda <k.gawda@alx.pl>
2022-04-09 11:10:41 +0200

    Usunięcie debug dump request

--- webserwisy/alpha/library/views.py
+++ webserwisy/alpha/library/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    context = {'title': "Pierwsza strona", 'dump': request}
+    context = {'title': "Pierwsza strona"}
     #print(request.path, request.method, request.META, request.headers)
     return render(request, 'library/index.html', context)
