2dc8ca043f70a8bcaaa98ab3b608362877e9867f
Konrad Gawda <k.gawda@alx.pl>
2022-04-10 14:28:50 +0200

    Komentarz - wybieranie obiektów w formularzu

--- webserwisy/alpha/library/forms.py
+++ webserwisy/alpha/library/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Book
+from .models import Book, Author
 
 class AddBookForm(forms.Form):
     title = forms.CharField(max_length=200)
     author_name = forms.CharField(max_length=200)
+    # Wybór obiektu? Lepiej tak, o ile nie jest ich wiele:
+    # Tylko pamiętajmy o zaimportowaniu Author
+    #author = forms.ModelChoiceField(Author.objects.all())
     description = forms.CharField(
         widget=forms.Textarea(attrs={'rows': 3, 'cols': 5}),
         required=False
