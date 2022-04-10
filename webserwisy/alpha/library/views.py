0c456aeafa43aad56a2035ed51c93ddd26c68955
Konrad Gawda <k.gawda@alx.pl>
2022-04-10 10:32:42 +0200

    Eksperyment z POST

--- webserwisy/alpha/library/views.py
+++ webserwisy/alpha/library/views.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Book, Author
 
@@ -14,13 +14,16 @@ def home(request):
         'books': books,
     }
     #print(request.path, request.method, request.META, request.headers)
+    print(request.method, request.POST)
     return render(request, 'library/index.html', context)
 
 def book(request, book_id):
-    book = Book.objects.get(id=book_id)
+    #book = Book.objects.get(id=book_id)
+    book = get_object_or_404(Book, id=book_id)
     return render(request, 'library/book.html', {'book': book})
 
 def author(request, author_id):
-    author = Author.objects.get(id=author_id)
+    #author = Author.objects.get(id=author_id)
+    author = get_object_or_404(Author, id=author_id)
     books = author.book_set.all()
     return render(request, 'library/author.html', {'author': author, 'books': books})
