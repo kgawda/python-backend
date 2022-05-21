0fd8ab97f2ad19e5e3e3e2c0f74a7cd11ba11a49
Konrad Gawda <k.gawda@alx.pl>
2022-05-21 15:51:39 +0200

    Login i logout dla auth

--- webserwisy/gamma/gamma/urls.py
+++ webserwisy/gamma/gamma/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include('buildingmanagement.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
